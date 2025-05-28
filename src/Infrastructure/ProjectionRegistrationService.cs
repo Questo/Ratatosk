1652e1d0d0e5b016538f29c98a6d52fcf06c2d70
Mathias Andreasen <mpma91@proton.me>
2025-05-28 22:01:57 +0200

    uow commit inside of projections because of scope

--- src/Infrastructure/ProjectionRegistrationService.cs
+++ src/Infrastructure/ProjectionRegistrationService.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ratatosk.Application.Shared;
 using Ratatosk.Core.Abstractions;
 
 namespace Ratatosk.Infrastructure;
@@ -11,26 +12,38 @@ public class ProjectionRegistrationService(IServiceProvider serviceProvider, IEv
         eventBus.Subscribe(async (domainEvent, ct) =>
         {
             using var scope = serviceProvider.CreateScope();
+            var provider = scope.ServiceProvider;
+            var uow = provider.GetRequiredService<IUnitOfWork>();
 
-            var domainEventType = domainEvent.GetType();
-            var projectionType = typeof(IDomainEventHandler<>).MakeGenericType(domainEventType);
-
-            var projections = scope.ServiceProvider.GetServices(projectionType);
-            foreach (var projection in projections)
+            try
             {
-                var whenAsyncMethod = projectionType.GetMethod("WhenAsync");
-                if (whenAsyncMethod is null)
+                var domainEventType = domainEvent.GetType();
+                var projectionType = typeof(IDomainEventHandler<>).MakeGenericType(domainEventType);
+
+                var projections = scope.ServiceProvider.GetServices(projectionType);
+                foreach (var projection in projections)
                 {
-                    continue;
+                    var whenAsyncMethod = projectionType.GetMethod("WhenAsync");
+                    if (whenAsyncMethod is null)
+                    {
+                        continue;
+                    }
+
+                    var task = (Task?)whenAsyncMethod.Invoke(projection, [domainEvent, ct]);
+                    if (task is null)
+                    {
+                        continue;
+                    }
+
+                    await task;
                 }
 
-                var task = (Task?)whenAsyncMethod.Invoke(projection, [domainEvent, ct]);
-                if (task is null)
-                {
-                    continue;
-                }
-
-                await task;
+                uow.Commit();
+            }
+            catch
+            {
+                uow.Rollback();
+                throw;
             }
         });
 
