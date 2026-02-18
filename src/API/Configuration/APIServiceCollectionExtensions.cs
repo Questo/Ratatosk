9bc4033ef5981cb292aab75bc245e4ebdc9db8bb
Mathias Andreasen <mpma91@proton.me>
2026-02-18 22:48:07 +0100

    feat: refactor API service registration and move authentication setup to a new extension method

---
+++ src/API/Configuration/APIServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ratatosk.Core.Primitives;
+using Ratatosk.Infrastructure.Configuration;
+
+namespace Ratatosk.API.Configuration;
+
+public static class APIServiceCollectionExtensions
+{
+    public static IServiceCollection AddAPI(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        services.AddOpenApi();
+
+        services.AddControllers();
+
+        var authOptions = configuration.GetSection(AuthOptions.SectionName).Get<AuthOptions>();
+        Guard.AgainstNull(authOptions, nameof(authOptions));
+        var key = Encoding.UTF8.GetBytes(authOptions!.Secret);
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = authOptions.Issuer,
+
+                    ValidateAudience = false,
+                    ValidAudience = authOptions.Audience,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                };
+            });
+        services.AddAuthorization();
+
+        return services;
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,52 +1,20 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Ratatosk.API.Auth;
 using Ratatosk.API.Products;
 using Ratatosk.API.Middleware;
 using Ratatosk.Application.Configuration;
-using Ratatosk.Core.Primitives;
 using Ratatosk.Infrastructure.Configuration;
+using Ratatosk.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddControllers();
-
-var authOptions = builder.Configuration.GetSection(AuthOptions.SectionName).Get<AuthOptions>();
-Guard.AgainstNull(authOptions, nameof(authOptions));
-var key = Encoding.UTF8.GetBytes(authOptions!.Secret);
-
-builder
-    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = authOptions.Issuer,
-
-            ValidateAudience = false,
-            ValidAudience = authOptions.Audience,
-
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-        };
-    });
-builder.Services.AddAuthorization();
+builder.Services.AddAPI(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -60,10 +28,13 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+// Register middleware
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseMiddleware<CleanupResponseMiddleware>();
+
+// Map endpoints
 app.MapGet("/healthz", () => Results.Ok("Healthy"));
 app.MapAuthEndpoints();
 app.MapProductsEndpoints();
